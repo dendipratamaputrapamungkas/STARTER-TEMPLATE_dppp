9f5215f3aa08f43fd6f1d4ea3dadc2db6b2b0546
dendipratamaputrapamungkas <dendipasper33@gmail.com>
2022-12-17 09:36:49 +0700

    fix di lab

--- resources/views/print_books.blade.php
+++ resources/views/print_books.blade.php
@@ -9,39 +9,43 @@
     <title>Print Book PDF</title>
 </head>
 <body>
-    <h1 class="text-center">Data Buku</h1>
-    <p class="text-center">Laporan Data Buku Tahun 2022</p>
-<br/>
-<table id="table-data" class="table table-bordered">
-    <thead>
-        <tr class="text-center">
-            <th>NO</th>
-            <th>Judul</th>
-            <th>Penulis</th>
-            <th>Tahun</th>
-            <th>Penerbit</th>
-            <th>Cover</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        @php $no=1; @endphp
-        @foreach ($books as $book)
+
+    <h1 class="text-center"> Data Buku</h1>
+    <p class="text-center">Laporan Buku Tahun 2022</p>
+    <br>
+    <table id="table-data" class="table table-bordered">
+        <thead>
             <tr>
-                <td>{{$no++}}</td>
-                <td>{{$book->judul}}</td>
-                <td>{{$book->penulis}}</td>
-                <td>{{$book->tahun}}</td>
-                <td>{{$book->penerbit}}</td>
-                <td>
-                    @if ($book->cover !== null)
-                        <img src="{{asset('storage/cover_buku/'.$book->cover)}}" width="100px"/>
-                    @else
-                    [Gambar tidak tersedia]
-                    @endif    
-                </td>
+                <th>NO</th>
+                <th>JUDUL</th>
+                <th>PENULIS</th>
+                <th>TAHUN</th>
+                <th>PENERBIT</th>
+                <th>COVER</th>
+
             </tr>
         </thead>
+        <tbody>
+            @php
+                $no = 1;
+            @endphp
+            @foreach ($books as $book)
+                <tr>
+                    <td>{{ $no++ }}</td>
+                    <td>{{ $book->judul }}</td>
+                    <td>{{ $book->penulis }}</td>
+                    <td>{{ $book->tahun }}</td>
+                    <td>{{ $book->penerbit }}</td>
+                    <td>
+                        @if ($book->cover !== null)
+                            <img src="{{ public_path('storage/cover_buku/'. $book->cover) }}" width="80px"/>
+                        @else
+                            [gambar tidak tersedia]
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 </body>
 </html>
